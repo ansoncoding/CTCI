fb82cc399b20352f5e825d34d8c9537f2130bde8
ansoncoding <53556427+ansoncoding@users.noreply.github.com>
2020-02-07 03:59:59 -0800

    Write simple tests for LinkedListS

--- C++/Practice_Pad/source/CH2/test_LinkedListS.cpp
+++ C++/Practice_Pad/source/CH2/test_LinkedListS.cpp
@@ -1,6 +1,64 @@
 #include "pch.h"
 #include "LinkedListString.h"
 
-TEST() {
+TEST(LinkedListStringTests, Init) {
+	LinkedListS lls = LinkedListS();
+	EXPECT_TRUE(lls.isEmpty());
+}
 
+TEST(LinkedListStringTests, PrependElements) {
+	LinkedListS lls = LinkedListS();
+	string s[] = { "!", "day", "beautiful", "a", "it's", "Hello"};
+	for (unsigned int i = 0; i < 6; i++) {
+		lls.prepend(s[i]);
+		EXPECT_TRUE(lls.contains(s[i]));
+		EXPECT_EQ(lls.getLength(), i + 1);
+		EXPECT_FALSE(lls.isEmpty());
+	}
+}
+
+TEST(LinkedListStringTests, RemoveElements) {
+	LinkedListS lls = LinkedListS();
+	string s[] = { "!", "day", "beautiful", "a", "it's", "Hello" };
+	for (unsigned int i = 0; i < 6; i++) {
+		lls.prepend(s[i]);
+	}
+	for (unsigned int i = 0; i < 6; i++) {
+		lls.remove(s[i]);
+		EXPECT_FALSE(lls.contains(s[i]));
+		EXPECT_EQ(lls.getLength(), 6 - i - 1);
+	}
+	EXPECT_TRUE(lls.isEmpty());
+}
+
+TEST(LinkedListStringTests, CopyConstructor) {
+	
+	LinkedListS lls = LinkedListS();
+	string s[] = { "!", "day", "beautiful", "a", "it's", "Hello" };
+	for (unsigned int i = 0; i < 6; i++) {
+		lls.prepend(s[i]);
+	}
+
+	LinkedListS copy(lls);
+	EXPECT_EQ(copy.getLength(), 6);
+	EXPECT_FALSE(copy.isEmpty());
+	for (unsigned int i = 0; i < 6; i++) {
+		EXPECT_TRUE(copy.contains(s[i]));	
+	}
+}
+
+TEST(LinkedListStringTests, AssignmentOperator) {
+
+	LinkedListS lls = LinkedListS();
+	string s[] = { "!", "day", "beautiful", "a", "it's", "Hello" };
+	for (unsigned int i = 0; i < 6; i++) {
+		lls.prepend(s[i]);
+	}
+
+	LinkedListS copy = lls;
+	EXPECT_EQ(copy.getLength(), 6);
+	EXPECT_FALSE(copy.isEmpty());
+	for (unsigned int i = 0; i < 6; i++) {
+		EXPECT_TRUE(copy.contains(s[i]));
+	}
 }
